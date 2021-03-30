cad03b9f39982db85a1ce4aec441e8ccc2b4c778
coutomariel <coutomariel@gmail.com>
2021-03-30 08:38:49 -0300

    Criado model de User e arquivo temporário de fake data

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,20 @@
+const DUMMY_USERS = {
+  '1' : const User(
+    id : "1",
+    nome : "Mariel",
+    email : "mariel@email.com",
+    avatarUrl : "https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png"
+  ),
+  '2' : const User(
+    id : "2",
+    nome : "Patricia",
+    email : "patricia@email.com",
+    avatarUrl : "https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png"
+  ),
+  '3' : const User(
+    id : "3",
+    nome : "Julia",
+    email : "julia@email.com",
+    avatarUrl : "https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_960_720.png"
+  ),
+};
